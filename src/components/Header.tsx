ef87654e765eb3b41b379fc3e8411d9eba9dfe0d
heitorlisboa <heitor01101000@gmail.com>
2022-06-23 00:57:47 -0300

    refactor: rename `Logo` component and remove the header h1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
-import { Logo } from './Logo';
+import { LogoIgniteLab } from '@/components/LogoIgniteLab';
 
 export function Header() {
   return (
     <header className="w-full py-5 flex justify-center bg-gray-700 border-b border-b-gray-600">
-      <h1>
-        <Logo />
-      </h1>
+      <LogoIgniteLab />
     </header>
   );
 }
