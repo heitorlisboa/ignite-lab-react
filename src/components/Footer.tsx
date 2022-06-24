ba60acdb13c2678fd100d8a9245fd08ddf98111b
heitorlisboa <heitor01101000@gmail.com>
2022-06-23 23:01:41 -0300

    feat: add responsiveness to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,19 @@ import { LogoRocketseat } from '@/components/LogoRocketseat';
 
 export function Footer() {
   return (
-    <footer className="text-gray-300 flex gap-6 items-center py-6 mx-6 border-t border-gray-500">
+    <footer
+      className="
+        text-gray-300
+        flex flex-col md:flex-row flex-wrap gap-6 items-center
+        2xl:max-w-7xl
+        py-6 mx-6 2xl:mx-auto border-t border-gray-500
+      "
+    >
       <a href="https://www.rocketseat.com.br" target="_blank" rel="noreferrer">
         <LogoRocketseat />
       </a>
       <span>Rocketseat - Todos os direitos reservados</span>
-      <a className="ml-auto hover:text-gray-200 transition-colors" href="#">
+      <a className="md:ml-auto hover:text-gray-200 transition-colors" href="#">
         Políticas de privacidade
       </a>
     </footer>
