6323aacac2b16bcecb90b9b1bfb06c429a3b0b5d
heitorlisboa <heitor01101000@gmail.com>
2022-06-25 16:02:57 -0300

    fix: add missing fallback for ISR

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import { CircleNotch } from 'phosphor-react';
+
+export function LoadingScreen() {
+  return (
+    <>
+      <Head>
+        <title>Carregando... &ndash; Ignite Lab</title>
+      </Head>
+
+      <div className="bg-gray-700 flex gap-2 items-center justify-center min-h-screen">
+        <CircleNotch className="motion-safe:animate-spin" size={24} />
+        <span className="text-2xl">Carregando...</span>
+      </div>
+    </>
+  );
+}
