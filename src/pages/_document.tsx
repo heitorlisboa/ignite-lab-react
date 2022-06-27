34fa6811e382d3d1934c070fa1bdbd4e3133e753
heitorlisboa <heitor01101000@gmail.com>
2022-06-26 22:13:12 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+        {/* Google Fonts stuff */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -14,6 +15,28 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        {/* Favicon stuff */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#03875f" />
+        <meta name="msapplication-TileColor" content="#03875f" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
